97bf9c7ccfe738a975764540465747406aa71a8b
OpenShift guest <zachary.mccain@sungard.com>
2013-12-04 15:08:10 -0500

    stubbing code

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ app.use(express.logger());
 app.use('/',express.static(__dirname+'/app/public'));
 app.listen(process.env.PORT);
 console.log('Express server started on port %s', process.env.PORT);
+
+app.get('/hello', function(req, res){
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
